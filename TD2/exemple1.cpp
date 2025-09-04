77885915b3212c3879fab1f8f4d31d1037778812
etud user <etud@port-hp-probook650G8-3>
2025-09-04 10:48:35 +0200

    TD2 04/09

---
+++ TD2/exemple1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int &x, int &y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+int main() {
+    int x = 10;
+    int y = 100;
+
+    swap(x, y);
+
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+
+    return 0;
+}
